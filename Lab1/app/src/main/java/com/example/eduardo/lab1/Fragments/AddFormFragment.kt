e503fbaef7c42b8cddcfa3c4a51258dc1af9bb31
Eduardo Rodriguez <erodriguez@abstract.cl>
2018-03-27 12:21:31 -0300

    Lab2 completo. La carpeta se llama Lab1 porque me base en el lab anterior para hacer este.

---
+++ Lab1/app/src/main/java/com/example/eduardo/lab1/Fragments/AddFormFragment.kt
@@ -0,0 +1,25 @@
+package com.example.eduardo.lab1.Fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.eduardo.lab1.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AddFormFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_form, container, false)
+    }
+
+}// Required empty public constructor
